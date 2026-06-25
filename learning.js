4575234ef552dac8822e43bd7a72f3b205aba3c5
Wanderson Pereira Fontes <wandersonwpk@gmail.com>
2026-06-25 00:39:09 -0300

    desafio

---
+++ learning.js
@@ -0,0 +1,20 @@
+let nomeDoHeroi = "Hagar"
+let nivelDoHeroi = 35
+if (nivelDoHeroi <=9) {
+    console.log("O herói " + nomeDoHeroi + " está no nível " + "F " + " e ainda tem muito a evoluir.")
+}
+else if (nivelDoHeroi >= 10 && nivelDoHeroi <= 19) {
+    console.log("O herói " + nomeDoHeroi + " está no nível " + "E " + " e ainda tem muito a evoluir.")
+}
+else if (nivelDoHeroi >= 20 && nivelDoHeroi <= 29) {
+    console.log("O herói " + nomeDoHeroi + " está no nível " + "D " + " e ainda tem muito a evoluir.")
+}
+else if (nivelDoHeroi >= 30 && nivelDoHeroi <= 39) {
+    console.log("O herói " + nomeDoHeroi + " está no nível " + "C " + " e ainda tem muito a evoluir.")
+}
+else if (nivelDoHeroi >= 40 && nivelDoHeroi <= 49) {
+    console.log("O herói " + nomeDoHeroi + " está no nível " + "B " + " e ainda tem muito a evoluir.")
+}
+else if (nivelDoHeroi >= 50 && nivelDoHeroi <= 59) {
+    console.log("O herói " + nomeDoHeroi + " está no nível " + "A " + " e é um verdadeiro mestre!") 
+}                                                          
